5340aeeea8690dc2682917aa8bb880fc0b7fa4ff
arpitkumar20990 <arpitchaudharychhillar777@gmail.com>
2026-03-20 15:01:07 +0530

    resolve the error

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import Hero from './components/Hero'
 import Categories from './components/Categories'
 import Groceries from './components/Groceries'
 import Banner from './components/Banner'
-import Cities from './components/cities'
+
 import Footer from './components/Footer'
+import Cities from './components/Cities'
 
 const App = () => {
   return (
